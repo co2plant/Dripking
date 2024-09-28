51eed408dbb941ff4eb3c9ee9e077a08575acf4a
co2plant <jaonz6057@wku.ac.kr>
2024-09-28 23:57:03 +0900

    # Initialize repository

---
+++ src/main/java/kr/co/inntavern/dripking/DripkingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.dripking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DripkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/inntavern/dripking/DripkingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.dripking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
