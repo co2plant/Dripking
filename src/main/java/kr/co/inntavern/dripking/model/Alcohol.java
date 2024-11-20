f6041b658c9c8378cb392ea787266fef5e3bec94
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-11-20 22:44:26 +0900

    # add model - Alcohol

---
+++ src/main/java/kr/co/inntavern/dripking/model/Alcohol.java
@@ -0,0 +1,44 @@
+package kr.co.inntavern.dripking.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Time;
+import java.util.Set;
+
+@Entity
+public class Alcohol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long alcohol_id;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column
+    private float strength;
+
+    @Column(length = 50)
+    private String stated_age;
+
+    @Column
+    private float size;
+
+    @Column(columnDefinition = "TEXT", length = 1000)
+    private String description;
+
+    @Column
+    private Time datetime;
+
+    @ManyToMany
+    @JoinTable(
+            name = "alcohol_tag",
+            joinColumns = @JoinColumn(name = "alcohol_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+
+    private Set<Tag> tags;
+}
