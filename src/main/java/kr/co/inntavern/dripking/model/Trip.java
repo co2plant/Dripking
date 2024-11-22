5e9bec931b8414f64ff2282db559e52678040081
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-11-22 09:42:08 +0900

    feat : Trip, User 모델 추가

--- src/main/java/kr/co/inntavern/dripking/model/Trip.java
+++ src/main/java/kr/co/inntavern/dripking/model/Trip.java
@@ -1,6 +1,5 @@
 package kr.co.inntavern.dripking.model;
 
-import com.fasterxml.jackson.annotation.JsonTypeId;
 import jakarta.persistence.*;
 
 import java.util.Date;
@@ -10,23 +9,16 @@ public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long trip_id;
+
     @Column
     private String name;
+
     @Column(length = 1000)
     private String description;
+
     @Column
     private Date start_date;
+
     @Column
     private Date end_date;
 }
-
-/*
-*
-* Trip{
-	trip_id Long PK
-	name varchar(255)
-	description text
-	start_date datetime
-	end_date datetime
-}
-* */
--- src/main/java/kr/co/inntavern/dripking/model/User.java
+++ src/main/java/kr/co/inntavern/dripking/model/User.java
@@ -1,4 +1,40 @@
 package kr.co.inntavern.dripking.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column
+    private String authentication_id;
+
+    @Column
+    private String authentication_pw;
+
+    @Column
+    private String name;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String address;
+
+    @Column(unique=true)
+    private String email_address;
 }
+
+/**
+ * User {
+ *     user_id Long PK
+ *     authentication_id Varchar(50)
+ *     authentication_pw Varchar(255)
+ *     name Varchar
+ *     phoneNumber String
+ *     address String
+ *     email_address String "unique"
+ * }
+ */
