7149deba7af4270c025e7dd51f8a563400efaf94
co2plant <93631841+co2plant@users.noreply.github.com>
2024-12-04 16:43:58 +0900

    fix : CORS가 적용이 안되던 오류 해결
    ### 오류 해결 방법
    - package의 위치를 변경하기위해 파일의 위치를src/main/java에서 src/main/java/kr/co/inntavern/dripking로 변경
    
    ** 스프링 애플리케이션에 해당 클래스를 등록하기위함 **

--- src/main/java/kr/co/inntavern/dripking/WebConfig.java
+++ src/main/java/kr/co/inntavern/dripking/WebConfig.java
@@ -1,13 +1,19 @@
+package kr.co.inntavern.dripking;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
