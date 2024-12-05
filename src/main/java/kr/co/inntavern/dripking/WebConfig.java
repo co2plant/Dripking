4d9a49185e5f82ee4ed57aa9f88de83d8e7f0faf
co2plant <93631841+co2plant@users.noreply.github.com>
2024-12-05 23:25:16 +0900

    fix : allowedMethod에서 PUT, DELETE 삭제

--- src/main/java/kr/co/inntavern/dripking/WebConfig.java
+++ src/main/java/kr/co/inntavern/dripking/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
