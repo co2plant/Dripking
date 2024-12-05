dcad565bf84fca94f2a4db8a972d7f4803d72623
co2plant <93631841+co2plant@users.noreply.github.com>
2024-12-05 23:27:53 +0900

    feat : AlcoholRepository 추가

---
+++ src/main/java/kr/co/inntavern/dripking/repository/AlcoholRepository.java
@@ -0,0 +1,14 @@
+package kr.co.inntavern.dripking.repository;
+
+import kr.co.inntavern.dripking.model.Alcohol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AlcoholRepository extends JpaRepository<Alcohol, Long> {
+    List<Alcohol> findAll();
+
+    Alcohol save(Alcohol alcohol);
+    Optional<Alcohol> findById(Long id);
+}
