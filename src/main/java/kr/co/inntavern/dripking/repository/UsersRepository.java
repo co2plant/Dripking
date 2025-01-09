5ba7478c3edf44d4b8ca9c3186893b66475b27cd
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2025-01-09 22:38:07 +0900

    fix : Id 서칭 추가

--- src/main/java/kr/co/inntavern/dripking/repository/UsersRepository.java
+++ src/main/java/kr/co/inntavern/dripking/repository/UsersRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UsersRepository extends JpaRepository<Users, Long> {
     Users save(Users users);
     Optional<Users> findByEmail(String email);
+    Optional<Users> findById(Long id);
 }
