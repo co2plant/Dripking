cfd5e2c7d4151274f0578a458ae97f1b1c31c43d
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2025-01-12 22:52:57 +0900

    feat : 임시 데이터 로더 픽스

--- src/main/java/kr/co/inntavern/dripking/DataLoader.java
+++ src/main/java/kr/co/inntavern/dripking/DataLoader.java
@@ -11,6 +11,13 @@ public class DataLoader {
     @Bean
     public CommandLineRunner loadData(DistilleryRepository distilleryRepository, AlcoholRepository alcoholRepository, DestinationRepository destinationRepository, CategoryRepository categoryRepository, TagRepository tagRepository, UsersRepository usersRepository, ReviewRepository reviewRepository) {
         return args -> {
+            Category categories = Category.builder()
+                    .name("Whisky")
+                    .description("맥아 효소로 녹말을 포함하고 있는 곡물 재료를 당화시키고 발효[5] 및 증류하여 오크통에 숙성시킨 증류주. 간단히 말해서 목통숙성곡물증류주(木桶熟成穀物蒸溜酒)라고 할 수 있다.")
+                    .build();
+
+            categoryRepository.save(categories);
+
             for (int i = 0; i < 100; i++) {
                 Destination destinations = Destination.builder()
                         .name("Destination " + i)
@@ -32,18 +39,13 @@ public class DataLoader {
                         .build();
                 tagRepository.save(tags);
 
-                Category categories = Category.builder()
-                        .name("Category " + i)
-                        .description("Description " + i)
-                        .build();
-
-                categoryRepository.save(categories);
-
                 Alcohol alcohols = Alcohol.builder()
                         .name("Alcohol " + i)
                         .strength(40 + i)
                         .size(700 + i)
                         .description("Alcohol Description " + i)
+                        .distillery(distilleryRepository.findById((long) 1).get())
+                        .category(categoryRepository.findById((long) 1).get())
                         .href("https://upload.wikimedia.org/wikipedia/commons/e/e5/Jim_Beam_White_Label.jpg")
                         .build();
                 alcoholRepository.save(alcohols);
@@ -65,6 +67,7 @@ public class DataLoader {
                         .build();
                 reviewRepository.save(reviews);
             }
+
         };
     }
 }
