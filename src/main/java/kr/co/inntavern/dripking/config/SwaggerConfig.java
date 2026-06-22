dabbcbfa59ab60e50d68d30e67d1866f3af94da8
co2plant <jaonz6057@gmail.com>
2026-06-23 00:28:55 +0900

    chore: swagger에서 authorize 버튼 누를 수 있도록 보안 스킴 추가

--- src/main/java/kr/co/inntavern/dripking/config/SwaggerConfig.java
+++ src/main/java/kr/co/inntavern/dripking/config/SwaggerConfig.java
@@ -6,17 +6,25 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
     private static final String API_NAME = "Dripking API";
     private static final String API_VERSION = "1.0.0";
     private static final String API_DESCRIPTION = "DripKing API Documentation";
+    private static final String BEARER_AUTH = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .info(new Info()
                         .title(API_NAME)
                         .version(API_VERSION)
